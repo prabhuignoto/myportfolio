964e229bc64ea935cd34c5f10b1da5d96979cebf
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2018-08-17 12:39:05 +0530

    removed jest-styled-components package

--- src/__tests__/snapshot-tests/builtwith-snap.js
+++ src/__tests__/snapshot-tests/builtwith-snap.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import BuiltWith from '../../components/builtwith';
-import 'jest-styled-components';
 
 it('aboutme renders correctly', () => {
   const wrapper = shallow(<BuiltWith tech={['react', 'typescript', 'gatsby']} />);
