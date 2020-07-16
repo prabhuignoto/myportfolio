b21eb1ba4d9f8465c3dd8a649b3665fa734dfdf3
Prabhu <prabhu.m.murthy@gmail.com>
2020-07-16 17:02:22 +0530

    updated resume link

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -6,22 +6,22 @@ export default function AboutMe() {
     <div
       className="box"
       style={{
-        position: 'relative',
-        backgroundColor: 'transparent',
-        boxShadow: 'none',
+        position: "relative",
+        backgroundColor: "transparent",
+        boxShadow: "none",
       }}
     >
       <About className="content">
-        <p>{'Hey I\'m Prabhu Murthy'}</p>
+        <p>{"Hey I'm Prabhu Murthy"}</p>
         <p>
-          {'I\'m a'}
-          <Mark>{' Fullstack'}</Mark>
-          {' javascript developer, designer and web applications architect.'}
+          {"I'm a"}
+          <Mark>{" Fullstack"}</Mark>
+          {" javascript developer, designer and web applications architect."}
         </p>
         <p>
           My expertise is building frontend apps using industry standard f/w &
           libs such as React, Vue, Angular, Svelte.
-          {' I also design & build application servers using '}
+          {" I also design & build application servers using "}
           <Mark>Node JS, Graph QL, Apollo Server & Sails JS</Mark>
         </p>
         <p>
@@ -32,13 +32,13 @@ export default function AboutMe() {
         <div>
           <a
             href="mailto:prabhu.m.murthy@gmail.com"
-            style={{ marginRight: '0.5rem', fontSize: '1.25rem' }}
+            style={{ marginRight: "0.5rem", fontSize: "1.25rem" }}
           >
             Contact me
           </a>
           <a
-            href="https://app.enhancv.com/share/cbbd06c3?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic"
-            style={{ marginRight: '0.25rem', fontSize: '1.25rem' }}
+            href="https://app.enhancv.com/share/0c0a2492?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic"
+            style={{ marginRight: "0.25rem", fontSize: "1.25rem" }}
             target="_new"
           >
             View Resume
